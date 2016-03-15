9d7433cc7a1685d0ee873893e31256662be9f019
Ashley Hutton <ashley.hutton@ku.edu>
2016-03-15 16:41:18 -0500

    CreateUser.php now adds new users to database

--- CreateUser.php
+++ CreateUser.php
@@ -1,13 +1,21 @@
+
 <?php
 
 $username = $_POST["username"];
 
+if (isset($_POST['submit'])){
+
+if ($username == ""){
+	echo "Username can't be blank!";
+}
+else {
+
 $dbURL = 'mysql.eecs.ku.edu';
 $dbUsername = 'ahutton';
-$dbPassword = 'myPassword';'
+$dbPassword = 'myPassword';
 $dbName = 'ahutton';
 
-/*
+
 $mysqli = new mysqli($dbURL, $dbUsername, $dbPassword, $dbName);
 
 if ($mysqli->connect_errno){
@@ -15,15 +23,26 @@ if ($mysqli->connect_errno){
 	exit();
 }
 
-$sql = "INSERT INTO Users (user_id) VALUES ($username)";
+$checkDuplicate = $mysqli->query("SELECT user_id FROM Users WHERE user_id='$username'");
 
-if (mysqli_query($mysqli, $sql){
+if ($checkDuplicate->num_rows === 0){
+
+$sql = "INSERT INTO Users (user_id) VALUES ('$username')";
+
+if ($mysqli->query($sql) === TRUE) {
 	echo "Success!";
 }
+
 else {
 	echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);
 }
+}
+else {
+	echo "Username already exists!";
+}
 
 mysqli_close($mysqli);
-*/
+}
+}
+
 ?>
