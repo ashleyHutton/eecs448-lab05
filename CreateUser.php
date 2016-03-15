61f1c4592961bdb0c883a5624b1d42762ebf8ff2
Ashley Hutton <ashley.hutton@ku.edu>
2016-03-15 15:54:15 -0500

    Integrate mysqli into CreateUser.php

--- CreateUser.php
+++ CreateUser.php
@@ -2,9 +2,28 @@
 
 $username = $_POST["username"];
 
-if (isset($_POST['submit'])){
+$dbURL = 'mysql.eecs.ku.edu';
+$dbUsername = 'ahutton';
+$dbPassword = 'myPassword';'
+$dbName = 'ahutton';
 
-	echo "well done";
+/*
+$mysqli = new mysqli($dbURL, $dbUsername, $dbPassword, $dbName);
+
+if ($mysqli->connect_errno){
+	printf("Connection Failed: %s\n", $mysqli->connect_error);
+	exit();
 }
 
+$sql = "INSERT INTO Users (user_id) VALUES ($username)";
+
+if (mysqli_query($mysqli, $sql){
+	echo "Success!";
+}
+else {
+	echo "Error: " . $sql . "<br>" . mysqli_error($mysqli);
+}
+
+mysqli_close($mysqli);
+*/
 ?>
